ecbed21ad1875641045b0682a3f934b560100bb5
realbigws <wangsheng@ttic.edu>
2019-11-04 00:27:28 -0600

    add install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#-> 1. create an conda environment 
+conda remove --name pointsite_inference --all -y
+conda create --name pointsite_inference python=3.6 -y
+conda activate pointsite_inference
+
+#-> 2. install related library
+pip install torch==1.1.0
+pip install torchvision==0.3.0
+conda install -c bioconda google-sparsehash -y
+conda install -c psi4 gcc-5 -y
+
+#-> 3. run setup
+rm -rf build/ dist/ sparseconvnet.egg-info sparseconvnet/SCN*.so
+python setup.py develop
+python setup_test.py
+rm -rf build/ dist/ sparseconvnet.egg-info sparseconvnet/SCN*.so
+rm -rf sparseconvnet/__pycache__
+
+#-> 4. deactivate
+conda deactivate
